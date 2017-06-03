6a9dec8d52a48787dfc2ba670c44beaa854363cf
MoriKen254 <masamorobo@gmail.com>
2017-06-03 09:31:46 +0900

    Add base files.

---
+++ include/pluginlib_arrayutil_sub/ave.h
@@ -0,0 +1,11 @@
+#include <pluginlib_arrayutil_base/arrayutil_base.h>
+
+namespace arrayutil_plugins 
+{
+  class Ave : public arrayutil_base::ArrayUtil
+  {
+    public:
+      Ave();
+      double operate();
+  };
+}
